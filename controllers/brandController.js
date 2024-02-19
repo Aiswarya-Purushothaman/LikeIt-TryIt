16b576196df24322614a862a188bc31c61b67eea
Aiswarya-Purushothaman <aiswaryapurushothamancp@gmail.com>
2024-02-19 19:57:28 +0530

    change the name of brand schema

--- controllers/brandController.js
+++ controllers/brandController.js
@@ -1,4 +1,4 @@
-const Brand = require("../models/brandschema")
+const Brand = require("../models/brandSchema")
 const Product = require("../models/productSchema")
 
 const getBrandPage = async (req, res)=>{
--- controllers/productController.js
+++ controllers/productController.js
@@ -1,6 +1,6 @@
 const Product = require("../models/productSchema");
 const Category = require("../models/categorySchema");
-const Brand = require("../models/brandschema");
+const Brand = require("../models/brandSchema");
 const fs = require("fs");
 const path = require("path");
 const User = require("../models/userSchema");
